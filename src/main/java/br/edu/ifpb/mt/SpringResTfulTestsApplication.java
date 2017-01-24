f863dc5a548214e91aacebf08c91d87711e52f9e
Franck Aragão <fjunior.aragao@gmail.com>
2017-01-24 17:32:46 -0300

    first commit

---
+++ src/main/java/br/edu/ifpb/mt/SpringResTfulTestsApplication.java
@@ -0,0 +1,12 @@
+package br.edu.ifpb.mt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringResTfulTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringResTfulTestsApplication.class, args);
+	}
+}
